5c10c96739abf3ed6e01ea1b88c24b227b75234b
Antoni Rościszewski <antoni.rosciszewski@gmail.com>
2016-07-19 17:00:43 +0200

    Refactored the project (in progress). Added mockup methods for ThinkServer client and two implementations (BMC power state, health state)

--- dracclient/wsman.py
+++ dracclient/wsman.py
@@ -18,7 +18,7 @@ from lxml import etree as ElementTree
 import requests
 import requests.exceptions
 
-from dracclient import exceptions
+import exceptions
 
 LOG = logging.getLogger(__name__)
 
@@ -34,6 +34,12 @@ NS_MAP = {'s': NS_SOAP_ENV,
           'wsa': NS_WS_ADDR,
           'wsen': NS_WSMAN_ENUM,
           'wsman': NS_WSMAN}
+
+NS_MAP_COMPUTER_SYSTEM = {'s': NS_SOAP_ENV,
+          'wsa': NS_WS_ADDR,
+          'wsen': NS_WSMAN_ENUM,
+          'wsman': NS_WSMAN,
+          'wsinst': 'http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_HostComputerSystem'}
 
 FILTER_DIALECT_MAP = {'cql': 'http://schemas.dmtf.org/wbem/cql/1/dsp0202.pdf',
                       'wql': 'http://schemas.microsoft.com/wbem/wsman/1/WQL'}
@@ -378,3 +384,49 @@ class _InvokePayload(_Payload):
                      {'resource_uri': self.resource_uri,
                       'name': name}))
                 property_elem.text = item
+
+
+class WSManClient(Client):
+    """Wrapper for Client with return value checking"""
+
+    def invoke(self, resource_uri, method, selectors=None, properties=None,
+               expected_return_value=None):
+        """Invokes a remote WS-Man method
+
+        :param resource_uri: URI of the resource
+        :param method: name of the method to invoke
+        :param selectors: dictionary of selectors
+        :param properties: dictionary of properties
+        :param expected_return_value: expected return value reported back by
+            the DRAC card. For return value codes check the profile
+            documentation of the resource used in the method call. If not set,
+            return value checking is skipped.
+        :returns: an lxml.etree.Element object of the response received
+        :raises: WSManRequestFailure on request failures
+        :raises: WSManInvalidResponse when receiving invalid response
+        :raises: DRACOperationFailed on error reported back by the DRAC
+                 interface
+        :raises: DRACUnexpectedReturnValue on return value mismatch
+        """
+        if selectors is None:
+            selectors = {}
+
+        if properties is None:
+            properties = {}
+
+        resp = super(WSManClient, self).invoke(resource_uri, method, selectors,
+                                               properties)
+
+        return_value = utils.find_xml(resp, 'ReturnValue', resource_uri).text
+        if return_value == utils.RET_ERROR:
+            message_elems = utils.find_xml(resp, 'Message', resource_uri, True)
+            messages = [message_elem.text for message_elem in message_elems]
+            raise exceptions.DRACOperationFailed(drac_messages=messages)
+
+        if (expected_return_value is not None and
+                return_value != expected_return_value):
+            raise exceptions.DRACUnexpectedReturnValue(
+                expected_return_value=expected_return_value,
+                actual_return_value=return_value)
+
+        return resp
